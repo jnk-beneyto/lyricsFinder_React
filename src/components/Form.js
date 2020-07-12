76ffcd0b3c32840ebd24d9aa71d6cbc6ba4ca351
J.C. Beneyto <jnk.beneyto@gmail.com>
2020-07-12 09:50:45 +0200

    completing Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,38 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import Error from "./Error";
+
 import "./form.css";
 
-const Form = () => {
+const Form = ({ setAllData }) => {
+	//setting state vars
+	const [error, setError] = useState(false);
+	const [artist, setArtist] = useState("");
+	const [song, setSong] = useState("");
+
+	const getData = e => {
+		e.preventDefault();
+
+		if (artist === "" || song === "") {
+			setError(true);
+			return;
+		}
+
+		//remove error
+		setError(false);
+
+		//set Correct data
+		setAllData({ artist, song });
+		console.log({ artist, song });
+	};
+
 	return (
 		<Fragment>
-			<div className="container form">
-				<form>
+			<div className="container form mt-3">
+				<form onSubmit={getData}>
 					<div className="row">
-						<div className="col-sm-12 col-md-5">
+						<div className="col-md-4 ">
 							<div className="form-group mb-2">
-								<label for="inputArtista" class="col-sm-2 col-form-label">
+								<label htmlFor="inputArtista" className="col-form-label">
 									Artista
 								</label>
 								<input
@@ -17,12 +40,15 @@ const Form = () => {
 									className="form-control"
 									id="inputArtista"
 									placeholder="Artista"
+									name="artist"
+									onChange={e => setArtist(e.target.value)}
+									value={artist}
 								/>
 							</div>
 						</div>
-						<div className="col-sm-12 col-md-5 mt-2 ">
+						<div className="col-md-4 ">
 							<div className="form-group mb-2">
-								<label for="inputCancion" class="col-sm-2 col-form-label">
+								<label htmlFor="inputCancion" className="col-form-label">
 									Canción
 								</label>
 								<input
@@ -30,10 +56,13 @@ const Form = () => {
 									className="form-control"
 									id="inputCancion"
 									placeholder="Canción"
+									name="song"
+									onChange={e => setSong(e.target.value)}
+									value={song}
 								/>
 							</div>
 						</div>
-						<div className="col-sm-12 col-md-2 d-flex">
+						<div className="col-md-4 d-flex">
 							<div className="container">
 								<button className="btn" type="submit">
 									Buscar
@@ -42,6 +71,11 @@ const Form = () => {
 						</div>
 					</div>
 				</form>
+			</div>
+			<div className="row justify-content-center">
+				<div className="col-md-4">
+					{error ? <Error message="rellena todo" /> : null}
+				</div>
 			</div>
 		</Fragment>
 	);
